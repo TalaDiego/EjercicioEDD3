7009890cbffd00f1bab04a17e6488af3f24c9e16
TalaDiego <TalaDiego@users.noreply.github.com>
2020-11-09 08:57:55 +0100

    Test commentary added and new class created
    I added a test comment and created a class called Square

---
+++ src/main/java/com/mycompany/ejercicioedd3/Square.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicioedd3;
+
+/**
+ *
+ * @author daw1
+ */
+public class Square {
+    
+}
